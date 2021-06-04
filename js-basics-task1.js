e6e49cf501f9c6a52ba31484b43cba7be9377011
Darkes116 <mrdarkespl@gmail.com>
2021-06-04 13:10:08 +0200

    task11 done

--- js-basics-task1.js
+++ js-basics-task1.js
@@ -3,10 +3,8 @@ const birthDateJs = new Date('09/18/1990');
 birthDateJs.setHours(11);
 const birthDate= JSON.stringify(birthDateJs);
 
-var num1=3;
-var num2=3;
-var num3=3;
-
+var num1,num2,num3;
+num1=num2=num3=3;
 
 console.log(`Hello! This program has been written by ${yourName}`);
 console.log(`Author has been born on ${birthDate}`);
